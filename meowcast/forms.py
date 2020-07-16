837c741397ebb086262bff25fc691b04cc949c40
dave <davec.vt@gmail.com>
2020-07-15 22:21:18 -0400

    yep

--- meowcast/forms.py
+++
@@ -1,7 +0,0 @@
-from wtforms import StringField, SubmitField
-from flask_wtf import FlaskForm
-
-
-class wxlookup(FlaskForm):
-    location = StringField('location', validators=None, description='enter location')
-    submit = SubmitField('MEOWCAST!!')
